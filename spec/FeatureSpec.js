50f0e65742ad897eaeadeba882e4b174638e4660
danieljonesdmj <danieljonesdmj@gmail.com>
2018-07-12 15:25:25 +0100

    implemented all business logic

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,23 +1,81 @@
 'use strict';
 
 describe('Feature test:', () => {
-  it('should start at 20 degrees',() => {
-    var thermostat = new Thermostat;
+  var thermostat;
+  var count;
+
+  beforeEach(function() {
+    thermostat = new Thermostat;
+  });
+
+  it('should start at 20 degrees', () => {
     expect(thermostat.temperature).toEqual(20);
   });
+
   it('The temperature can be increased', () => {
-    var thermostat = new Thermostat;
     thermostat.up();
     expect(thermostat.temperature).toEqual(21);
-  })
+  });
+
   it('The temperature can be decreased', () => {
-    var thermostat = new Thermostat;
     thermostat.down();
     expect(thermostat.temperature).toEqual(19);
-  })
+  });
+
   it('Has a minimum temperature of 10 degrees', () => {
-    var thermostat = new Thermostat;
-    spyOn(thermostat, 'temperature').and.returnValue(9)
+    for (count = 0; count < 10; count++) {
+      thermostat.down();
+    }
     expect(function() {thermostat.down();}).toThrowError('NOOO ITS COLD');
-  })
+  });
+
+  it('Does not throw an error when the temperature is 10 or above', () => {
+     for (count = 0; count < 9; count++) {
+       thermostat.down();
+     }
+     expect(function() {thermostat.down();}).not.toThrowError('NOOO ITS COLD');
+   });
+
+  it('It has a power saving mode, which is on at default', () => {
+    expect(thermostat.powersavingmode).toBeTruthy();
+  });
+
+  it('Power saving mode can be turned off', () => {
+    thermostat.powerSavingModeOff();
+    expect(thermostat.powersavingmode).toBeFalsy();
+  });
+
+  it('Power saving mode sets the maximum temperature at 25', () => {
+    thermostat.temperature = 25;
+    expect(function() {thermostat.up();}).toThrowError('At maximum temperature');
+  });
+
+  it('Maximum temperature is 32 degrees when power saving mode is on', () => {
+    thermostat.powerSavingModeOff();
+    thermostat.temperature = 32;
+    expect(function() {thermostat.up();}).toThrowError('At maximum temperature');
+  });
+
+  it('Can reset the temperature to 20 degrees', () => {
+    thermostat.temperature = 22;
+    thermostat.reset();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('Displays current energy usage', () => {
+    thermostat.energy();
+    expect(thermostat.energy).toEqual('medium-usage');
+  });
+
+  it('Displays low-usage when temperature below 18 degrees', () => {
+    thermostat.temperature = 16;
+    thermostat.energy();
+    expect(thermostat.energy).toEqual('low-usage');
+  });
+
+  it('Displays low-usage when temperature below 18 degrees', () => {
+    thermostat.temperature = 27;
+    thermostat.energy();
+    expect(thermostat.energy).toEqual('high-usage');
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,22 +1,50 @@
 'use strict';
 
 function Thermostat() {
-this.temperature = 20;
-};
-
-Thermostat.prototype.temperature = function() {
-  this.temperature;
+  this.temperature = 20;
+  this.powersavingmode = true;
 }
 
 Thermostat.prototype.up = function() {
-  this.temperature += 1
-  return this.temperature
+  if (this.powersavingmode === true && this.temperature < 25) {
+    this.temperature += 1;
+    return this.temperature;
+} else if (this.powersavingmode !== true && this.temperature < 32) {
+    this.temperature += 1;
+    return this.temperature;
+} else {
+  throw new Error ('At maximum temperature');
+}
 };
 
 Thermostat.prototype.down = function() {
-  if (this.temperature >= 10) {
-    this.temperature -= 1
+  if (this.temperature > 10) {
+    this.temperature -= 1;
+    return this.temperature;
+  } else {
+    throw new Error ('NOOO ITS COLD');
+  }
+};
+
+Thermostat.prototype.powerSavingModeOn = function() {
+    this.powersavingmode = true;
+};
+
+Thermostat.prototype.powerSavingModeOff = function() {
+    this.powersavingmode = false;
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+  return this.temperature;
+};
+
+Thermostat.prototype.energy = function() {
+  if (this.temperature < 18 ) {
+    this.energy = 'low-usage';
+} else if (this.temperature >= 25 ) {
+    this.energy = 'high-usage';
 } else {
-  throw new Error ('NOOO ITS COLD')
-}
+    this.energy = 'medium-usage';
+    }
 };
